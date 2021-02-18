91094e431191cb2dafd868d0ad9198716f91fdec
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2021-02-18 18:56:09 -0500

    Rutas y componentes principales del frontal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Route, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppComponent } from './app.component';
+import { BalanceComponent } from './pages/balance/balance.component';
+import { SeguimientoComponent } from './pages/seguimiento/seguimiento.component';
+
+// Rutas
+const Routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'balance' },
+  { path: 'balance', component: BalanceComponent },
+  { path: 'seguimiento', component: SeguimientoComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'balance' },
+]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent, BalanceComponent, SeguimientoComponent],
+  imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot(Routes, { useHash: true, scrollOffset: [0, 0] })],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/pages/balance/balance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './balance.component.html',
+  styles: [
+  ]
+})
+export class BalanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
