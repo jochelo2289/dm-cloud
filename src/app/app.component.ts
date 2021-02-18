74c5c8eac8de87863b5dec57291f9a5274ea3057
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2021-02-18 16:56:03 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jlps-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'md-cloud';
+}
