b0f6add5546e80055d1c9ee9caeeedf7e1685be0
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2021-02-19 18:30:26 -0500

    Formulario para la consulta del balance.

--- src/app/pages/balance/balance.component.ts
+++ src/app/pages/balance/balance.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Reports, FilesReport, Tasks } from '@app/mocks/data.mock';
 @Component({
   templateUrl: './balance.component.html',
   styles: []
 })
 export class BalanceComponent {
-  visible = true;
+  visible = false;
 
   reports = Reports;
   dataSource = FilesReport;
@@ -13,5 +14,27 @@ export class BalanceComponent {
 
   tabsPanel = [{ title: 'Seguimiento' }, { title: 'Archivos' }];
 
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formBuild();
+  }
+
   actionButton = (event: string) => alert(event);
+
+  onSubmit = () => {
+    if (this.formulario.valid) {
+      this.visible = true;
+    }
+  };
+
+  private formBuild = () => {
+    this.formulario = this.fb.group({
+      operadora: ['', Validators.required],
+      contrato: ['', Validators.required],
+      fecha: ''
+    });
+
+    this.formulario.valueChanges.subscribe(() => (this.visible = false));
+  };
 }
